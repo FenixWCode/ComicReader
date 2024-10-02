9c92567ab52ab70aa73691f47e9870cac03bc876
Felix <felix@warmuth.tv>
2024-10-02 02:51:00 +0200

    Added keybindings

--- reader.py
+++ reader.py
@@ -1,15 +1,19 @@
 from tkinter import filedialog as fd
-import tkinter as tk
-from tkinter import ttk
 import os
 import customtkinter as ctk
-from customtkinter import CTk
-from PIL import Image, ImageTk
-import re
+from PIL import Image
 
 
-width = 1500
-height = 2000
+width = 1800
+height = 2700
+
+
+def right_arrow_pressed(event):
+    next_page(f_list, cur_index)
+
+
+def left_arrow_pressed(event):
+    prev_page(f_list, cur_index)
 
 
 def next_page(file_list, index):
@@ -19,7 +23,6 @@ def next_page(file_list, index):
     image_ctk = ctk.CTkImage(light_image=image, size=(width, height))
     label.configure(image=image_ctk)
     cur_index += 1
-    print(index)
 
 
 def prev_page(file_list, index):
@@ -29,7 +32,6 @@ def prev_page(file_list, index):
     image_ctk = ctk.CTkImage(light_image=image, size=(width, height))
     label.configure(image=image_ctk)
     cur_index -= 1
-    print(index)
 
 
 def list_dir(file):
@@ -47,6 +49,12 @@ app._state_before_windows_set_titlebar_color = 'zoomed'
 
 app.grid_rowconfigure(0, weight=1)
 app.grid_columnconfigure(0, weight=1)
+app.bind('<KeyRelease-Right>', right_arrow_pressed)
+app.bind('<KeyPress-Left>', left_arrow_pressed)
+
+screen_width = app.winfo_screenwidth()
+screen_height = app.winfo_screenheight()
+
 
 # Pick an Image
 filename = fd.askopenfilename(initialdir="C:\D\BILDER\Iwas\Bilder\Comics\Selenium", title="Select Image")
@@ -58,16 +66,16 @@ cur_index = f_list.index(name)
 first_image = Image.open(filename)
 first_image_ctk = ctk.CTkImage(light_image=first_image, size=(width, height))
 
-my_frame = ctk.CTkScrollableFrame(master=app, width=width, height=height, corner_radius=0, fg_color="transparent")
-my_frame.grid(row=0, column=0, sticky="n")
+my_frame = ctk.CTkScrollableFrame(master=app, width=2560, height=3000, corner_radius=0, fg_color="transparent")
+my_frame.grid(row=0, column=0, padx=20, sticky="nsew")
 
 label = ctk.CTkLabel(my_frame, text="", image=first_image_ctk)
-label.grid(row=0, column=0, padx=20)
+label.grid(row=0, column=0, padx=350, sticky="e")
 
-next_button = ctk.CTkButton(app, text="", command=lambda: next_page(f_list, cur_index))
-next_button.grid(row=0, column=0, sticky="e")
+next_button = ctk.CTkButton(app, text=">>>", command=lambda: next_page(f_list, cur_index))
+next_button.grid(row=0, column=0, padx=50, sticky="e")
 
 prev_button = ctk.CTkButton(app, text="<<<", command=lambda: prev_page(f_list, cur_index))
-prev_button.grid(row=0, column=0, sticky="w")
+prev_button.grid(row=0, column=0, padx=50, sticky="w")
 
 app.mainloop()
